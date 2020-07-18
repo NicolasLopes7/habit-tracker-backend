fe789d77b31af49336b5348ba90fdf2f6443796e
Nicolas Lopes Aquino <nicolaslopesbusiness@gmail.com>
2020-07-18 18:52:41 -0300

    adding dotenv

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const cors = require("cors");
 const routes = require("./src/routes");
 const app = express();
 
+require("dotenv").config();
+
 app.use(express.json());
 app.use(cors());
 app.use(routes);
 
+const PORT = process.env.PORT || 5000;
 app.listen(process.env.PORT || 5000);
